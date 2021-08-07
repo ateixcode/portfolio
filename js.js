e2eac33036ca8af413bcfdbeffca0ab0dc8e3943
ateixcode <teixeira.alejandro.cebas@gmail.com>
2021-08-07 19:53:21 -0300

    First commit, original website files

---
+++ js.js
@@ -0,0 +1,6 @@
+function saludar() {
+    alert("Hola!, este es mi sitio web y aún está en desarrollo, gracias por tu visita!");
+    alert("Hi! this is my website and it's still under development, thank you for your visit!");
+}
+
+saludar();
