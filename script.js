1d067d85050941dcfb6fb190f77c883908c0064c
ateixcode <teixeira.alejandro.cebas@gmail.com>
2021-08-07 22:19:00 -0300

    Cambios finales para entrega

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// function saludar() {
-//     alert("Hola!, este es mi sitio web y aún está en desarrollo, gracias por tu visita!");
-//     alert("Hi! this is my website and it's still under development, thank you for your visit!");
-// }
+function saludar() {
+    alert("Hola!, este es mi sitio web y aún está en desarrollo, gracias por tu visita!");
+    alert("Hi! this is my website and it's still under development, thank you for your visit!");
+}
 
-// saludar();
+saludar();
